371b605258db30d8a81df6c12d6f0877965d7d9b
zburkard <burkard.exe@gmail.com>
2023-03-20 17:58:51 -0600

    paragraph component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Paragraph from './components/ui/Paragraph'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <main className='bg-red-500'>hello world</main>
+  return <main className='bg-red-500'><Paragraph size='sm'/></main>
   
 }
